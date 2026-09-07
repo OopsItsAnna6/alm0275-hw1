1206003fd7df149d162f4b729ff4ccbaeb28cc3c
OopsItsAnna6 <fw52ng92nw@privaterelay.appleid.com>
2026-09-06 19:53:19 -0500

    Update homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+  int a, b, c;
+
+  a = 1;
+  b = 2;
+  c = 3;
+
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+  cout << "c = " << c << endl;
+
+  return 0;
+}
